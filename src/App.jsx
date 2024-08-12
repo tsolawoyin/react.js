61796dec9e1f2ef6f1a1bee42f1a25458c6cbc0b
tsolawoyin <t.olawoyin@outlook.com>
2024-08-12 21:56:35 +0100

    In progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
+import Header from "./components/Header"
 
 function App() {
   return (
-    <h1>CV generator</h1>
+    <Header />
   )
 }
 
 export default App
-
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header () {
+    return (
+        <div>
+            <div id="logo">CV</div>
+            <div id="tabs">
+                <li>Bournvita resume</li>
+                <li>Peak milk</li>
+                <li></li>
+            </div>
+        </div>
+    )
+}
+
+export default Header
