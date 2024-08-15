5a4bf201a8a9789a55a1f9ceaf5b2327357aa570
tsolawoyin <t.olawoyin@outlook.com>
2024-08-15 07:41:18 +0100

    In progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,73 @@
+import { useState } from 'react';
+import { v4 as randomID } from 'uuid';
+
+import '../css/Header.css';
+
+let tabList = [
+    {
+        isActive: false,
+        title: "Yummy resume",
+        key: randomID()
+    },
+    {
+        isActive: true,
+        title: "Bournvita resume",
+        key: randomID()
+    },
+    {
+        isActive: false,
+        title: "Nunu resume",
+        key: randomID()
+    },
+    {
+        isActive: false,
+        title: "Titus resume",
+        key: randomID()
+    },
+    {
+        isActive: false,
+        title: "Chivita resume",
+        key: randomID()
+    },
+]
 
 function Header () {
+    // tomorrow we useImmer.
+    let [tabs, setTabs] = useState(tabList);
+    // we need to update this stuff
+    function handleClick( e ) {
+        let id = e.target.id;
+
+        setTabs(tabs.map(tab => {
+            // turn off any existing one
+            if (tab.isActive) {
+                return {...tab, isActive: false};
+            }
+            // then check others...
+            if (tab.key == id) {
+                return {...tab, isActive: true}
+            } else {
+                return tab;
+            }
+        }))
+        
+    }
+    let resumes = tabs.map(e => {
+        return (
+            <li key={e.key} onClick={handleClick} >
+                <span className={e.isActive ? "tab-name active" : "tab-name"} id={e.key}>{e.title}</span>
+                <span className='delete is-medium'></span>
+            </li>
+        )
+    })
     return (
-        <div>
+        <header className='p-2' id='header'>
             <div id="logo">CV</div>
-            <div id="tabs">
-                <li>Bournvita resume</li>
-                <li>Peak milk</li>
-                <li></li>
-            </div>
-        </div>
+            <ul id="tabs">
+                {resumes}
+            </ul>
+            <button className='button is-small'>Dark</button>
+        </header>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-// import "bulma.css"
+import "bulma/css/bulma.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
